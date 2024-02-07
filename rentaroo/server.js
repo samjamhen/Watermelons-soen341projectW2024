2b30e1972076f3fbc8d61836f282efc808ab09a3
Benjamin Ho <benjaminho2470@hotmail.com>
2024-02-07 18:40:25 -0500

    Added proxy configuration for backend server

--- rentaroo/server.js
+++ rentaroo/server.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
+const PORT = process.env.PORT || 8000;
 
 //Connect to MongoDB and start the server
 mongoose.connect('mongodb+srv://admin:Rentaroo1@rentaroodb.dchkjpr.mongodb.net/?retryWrites=true&w=majority')
 .then(() => {
     console.log("Connected to MongoDB")
-    app.listen(3000, () => {
-        console.log("Rentaroo app is running on port 3000")
+    app.listen(PORT, () => {
+        console.log(`Rentaroo app is running on port ${PORT}`)
     })
 }).catch((error) => {
     console.log(error)
