372aba19dde5a8462f23bd63e2299c0031ef24f7
Ikram Kamal <kamalikram65@gmail.com>
2024-02-08 23:10:03 -0500

    Route for Admin page

--- rentaroo/src/App.js
+++ rentaroo/src/App.js
@@ -3,8 +3,9 @@ import Home from './pages/Home';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import CatalogPage from './pages/CatalogPage';
 import Admin from './pages/Admin';
-import ClientManagement from './pages/ClientManagement';
-
+import ClientsManagement from './pages/ClientsManagement';
+import VehiclesManagement from './pages/VehiclesManagement';
+import ReservationsManagement from './pages/ReservationsManagement';
 
 
 function App() {
@@ -20,6 +21,12 @@ function App() {
           <Route path="/Catalog" element={<CatalogPage />} /> 
           <Route path="/Home" element={<Home />} /> 
           <Route path="/Admin" element={<Admin />} /> 
+        </Routes>
+
+        <Routes>
+          <Route  path="/ClientsManagement" element={<ClientsManagement/>} />
+          <Route  path="/ReservationsManagement" element={<ReservationsManagement/>} />
+          <Route  path="/VehiclesManagement" element={<VehiclesManagement/>} />
         </Routes>
       </div>
     </Router>
