914f914ad53f4f1f4830768f5dd96f34f1a0d0c7
Ikram Kamal <kamalikram65@gmail.com>
2024-02-23 13:55:50 -0500

    install Packages for Jest

---
+++ rentaroo/src/components/__test__/Footer.test.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import Footer from '../Footer';
+
+test('renders Footer component without crashing', () => {
+  render(<Footer />);
+});
