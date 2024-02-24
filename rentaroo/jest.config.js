cfe5843bb9697c52103e28366e6daf6593870086
Uyi Oronsaye <uoronsaye@gmail.com>
2024-02-24 14:51:35 -0500

    running tests

---
+++ rentaroo/jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  roots: ["<rootDir>/src"], // Specify the root directory that Jest should scan for tests and modules within
+  transform: {
+    "^.+\\.jsx?$": "babel-jest",
+  },
+  moduleNameMapper: {
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+  },
+  testEnvironment: "jsdom", // Specify the test environment as "jsdom"
+  setupFilesAfterEnv: ["./src/setupTests.js"], // Set up additional matchers provided by @testing-library/jest-dom
+};
