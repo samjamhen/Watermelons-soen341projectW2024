f2ea20307a6d8c78541baefdfc0f33a23270d546
Ikram Kamal <kamalikram65@gmail.com>
2024-03-20 12:45:23 -0400

    Map placeholder

--- rentaroo/src/components/Map.jsx
+++ rentaroo/src/components/Map.jsx
@@ -1,15 +1,27 @@
-import React, {useState} from 'react';
+import React from 'react';
 
-const Map= () =>{
-
-
-
-return(
-   <h1>Map View</h1>
-);
-
-
-
+const Map = () => {
+  return (
+    <div>
+      <h1>Map View</h1>
+      
+      
+      {/* This code is just a placeholder and should be removed after implementing the map API.
+      The code only provides a map frame. This was made for frontend purposes only. */}
+      <div>
+        <iframe
+          title="Map View"
+          width="100%"
+          height="600"
+          frameborder="0"
+          scrolling="no"
+          marginheight="0"
+          marginwidth="0"
+          src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=Montreal+(Rentaroo)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
+        ></iframe>
+      </div>
+    </div>
+  );
 };
 
 export default Map;
