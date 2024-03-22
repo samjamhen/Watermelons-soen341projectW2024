11ff68a8f8b9946d20ed51002c1bf83e70a3fc46
y-mouatif <117135148+y-mouatif@users.noreply.github.com>
2024-03-22 17:28:18 -0400

    Confirm return page linked to Payment Settlement Page

---
+++ rentaroo/src/pages/Service_rep/PaymentCheckout.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { useLocation } from 'react-router-dom';
+import Header from '../../components/HeaderCSR';
+import Footer from '../../components/Footer';
+import PaymentSettlement from '../../components/PaymentSettlement';
+
+const PaymentCheckout = () => {
+    const location = useLocation();
+    const fetchedReservation = location.state.fetchedReservation;
+
+    return (
+        <div>
+            <Header />
+            <div>
+                <h3>Reservation Details:</h3>
+                <p><strong>Full Name:</strong> {fetchedReservation.fullName}</p>
+                <p><strong>Email:</strong> {fetchedReservation.email}</p>
+                <p><strong>Return Date:</strong> {fetchedReservation.returnDate}</p>
+            </div>
+            <div className="checkout-container">
+                <h1>Payment Checkout Page</h1>
+                {/* <PaymentSettlement /> */}
+            </div>
+            <Footer />
+        </div>
+    );
+};
+
+export default PaymentCheckout;
