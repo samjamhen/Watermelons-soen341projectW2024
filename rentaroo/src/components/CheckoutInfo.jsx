fd84b280a83f341fd3343d8d460ca4dd1f222e47
y-mouatif <117135148+y-mouatif@users.noreply.github.com>
2024-03-21 22:22:17 -0400

    Added style to the checkout search

--- rentaroo/src/components/CheckoutInfo.jsx
+++ rentaroo/src/components/CheckoutInfo.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import "../styles/checkoutInfo.css";
 
 function CheckoutInfo() {
     const [searchType, setSearchType] = useState('confirmationNumber');
